8d4fa2948d0f3fc018fe42e00e5f0da92c9f5e3e
Caio Rodrigues <caio.rodrigues@zup.com.br>
2021-05-26 21:05:55 -0300

    Setup do projeto

---
+++ src/main/java/com/br/zupacademy/api/mercadolivre/DesafioMercadoLivreApplication.java
@@ -0,0 +1,13 @@
+package com.br.zupacademy.api.mercadolivre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMercadoLivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMercadoLivreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/zupacademy/api/mercadolivre/DesafioMercadoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.zupacademy.api.mercadolivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioMercadoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
